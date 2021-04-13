1042d37bf44e27111a6c26ee36992d7f73151599
zeineb-eya <zeineb.rahmani@esprit.tn>
2021-04-13 21:06:57 +0100

    first html commit

---
+++ connexion.php
@@ -0,0 +1,34 @@
+
+   <?php
+ include "config.php";
+
+class service
+{
+	public $nom;
+	public $prix;
+}
+
+
+
+ function connexion($post) {
+            try {
+                $pdo = getConnexion();
+                $query = $pdo->prepare(
+                    'INSERT INTO services (nom,prix)
+                VALUES (:nom, :prix)'
+                );
+                $query->execute([
+                    'nom' => $post->nom,
+                    'prix' => $post->prix
+                ]);
+                echo "Posted Successfully";
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+$post=new service ();
+$post->nom= $_POST["nom"];
+$post->prix= $_POST["prix"];
+
+connexion($post);
+?>
