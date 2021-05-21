0cf6703959910fa5a5532e2972bafe8525516467
Maryem Ben Massaoud <maryem.benmassoud@esprit.tn>
2021-05-21 21:33:23 +0100

    IntegrF

--- phpharoun/produit/views/afficherproduit.php
+++ phpharoun/produit/views/afficherproduit.php
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 
 
 
@@ -35,41 +34,3 @@ foreach($listeproduits as $row){
 }
 ?>
 </table>
-=======
-
-
-
-<?PHP
-
-include __DIR__."/../core/produitC.php";
-$produit1C=new produitC();
-$listeproduits=$produit1C->afficherproduits();
-
-?>
-<table>
-<tr>
-	<td>nom</td>
-	<td>prix</td>
-	<td>ajouter au panier</td>
-    </tr>
-<?PHP
-foreach($listeproduits as $row){
-	
-	?>
-	<tr>
-	<td><?= $row['nom'] ?></td>
-	
-	<td><?=$row['prix']?>TND</td>
-	<td>
-		
-		<a href="http://localhost/haroun/php/panier/views/ajouterpanier.php?id=<?=$row['id']?>">ajouter au panier</a>
-	</td>
-    </tr>
-	
-	
-<?php	
-
-}
-?>
-</table>
->>>>>>> .theirs
